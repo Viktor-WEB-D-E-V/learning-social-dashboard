799ed0ce184997f413439da207c66cab4f210ead
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-29 21:40:28 -0900

    Created a <Profile> component to display user information

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import userData from "./utils/userData.json";
 
+import Profile from "./components/Profile/Profile";
+Profile;
 function App() {
-
   return (
     <>
+      <Profile {...userData} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
